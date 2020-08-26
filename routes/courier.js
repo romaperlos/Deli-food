fb2fa16716fe9b4a3d9e5ae2569750cdf0078e75
Roma RedDragon <roma.perlos@gmail.com>
2020-08-26 18:44:16 +0300

    Курьер и Юзер views и hbsess

---
+++ routes/courier.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const Courier = require('../models/courier.js');
+
+
+
+module.exports = router;
---
+++ routes/user.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+const User = require('../models/user.js');
+
+router.get('/', (req, res) => {
+
+})
+
+module.exports = router;
